3208d35009958d9e0b1eceb764dc897b45d67539
arnobl <1052622+arnobl@users.noreply.github.com>
2019-11-06 18:02:12 +0100

    test(binding): html code fixed

--- test/binding/ComboboxBinder.test.ts
+++ test/binding/ComboboxBinder.test.ts
@@ -25,9 +25,9 @@ let disposable: Subscription;
 beforeEach(() => {
     document.documentElement.innerHTML =
         "<html><div><input id='comb1' list='test'><datalist id='test1'>" +
-        "<option value='test1a'>Test1a</option><option value='test2a'>Test2a</option> </datalist>" +
+        "<option value='test1a'>Test1a</option><option value='test2a'>Test2a</option> </datalist></input>" +
         "<input id='comb2' list='test'><datalist id='test2'>" +
-        "<option value='test1b'>Test1b</option><option value='test2b'>Test2b</option> </datalist></div></html>";
+        "<option value='test1b'>Test1b</option><option value='test2b'>Test2b</option> </datalist></input></div></html>";
     const elt1 = document.getElementById("comb1");
     if (elt1 !== null) {
         widget1 = elt1;
