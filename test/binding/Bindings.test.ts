8afa3e0ad05a7b621cfd1ce0bf474179cb578f89
arnobl <1052622+arnobl@users.noreply.github.com>
2020-09-08 22:53:23 +0200

    test(binding): tests added

---
+++ test/binding/Bindings.test.ts
@@ -0,0 +1,122 @@
+/*
+ * This file is part of Interacto.
+ * Interacto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Interacto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Interacto.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {
+    clickBinder,
+    CommandsRegistry,
+    dbleClickBinder, dndBinder, dragLockBinder,
+    EventRegistrationToken, keyPressBinder, keyTypeBinder,
+    pressBinder,
+    UndoCollector
+} from "../../src/interacto";
+import {StubCmd} from "../command/StubCmd";
+import {Subscription} from "rxjs";
+import {createKeyEvent, createMouseEvent} from "../interaction/StubEvents";
+
+let elt: HTMLElement;
+let producedCmds: Array<StubCmd>;
+let disposable: Subscription;
+
+beforeEach(() => {
+    document.documentElement.innerHTML = "<html><div id='div'></div></html>";
+    const elt1 = document.getElementById("div");
+    if (elt1 !== null) {
+        elt = elt1;
+    }
+    producedCmds = [];
+});
+
+afterEach(() => {
+    disposable.unsubscribe();
+    CommandsRegistry.getInstance().clear();
+    UndoCollector.getInstance().clear();
+});
+
+test("press binder", () => {
+    const binding = pressBinder()
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, elt));
+    expect(producedCmds).toHaveLength(1);
+});
+
+test("click binder", () => {
+    const binding = clickBinder()
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    expect(producedCmds).toHaveLength(1);
+});
+
+test("double click binder", () => {
+    const binding = dbleClickBinder()
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    expect(producedCmds).toHaveLength(1);
+});
+
+test("drag lock binder", () => {
+    const binding = dragLockBinder()
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseMove, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.Click, elt));
+    expect(producedCmds).toHaveLength(1);
+});
+
+test("dnd binder", () => {
+    const binding = dndBinder(false, false)
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseMove, elt));
+    elt.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, elt));
+    expect(producedCmds).toHaveLength(1);
+});
+
+test("key press binder", () => {
+    const binding = keyPressBinder(false)
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createKeyEvent(EventRegistrationToken.KeyDown, "A"));
+    expect(producedCmds).toHaveLength(1);
+});
+
+test("key type binder", () => {
+    const binding = keyTypeBinder()
+        .on(elt)
+        .toProduce(() => new StubCmd(true))
+        .bind();
+    disposable = binding.produces().subscribe(c => producedCmds.push(c));
+    elt.dispatchEvent(createKeyEvent(EventRegistrationToken.KeyDown, "A"));
+    elt.dispatchEvent(createKeyEvent(EventRegistrationToken.KeyUp, "A"));
+    expect(producedCmds).toHaveLength(1);
+});
