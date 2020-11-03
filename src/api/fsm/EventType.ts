95195345b75af94b7a799992debe0d5b55a900ec
arnobl <1052622+arnobl@users.noreply.github.com>
2020-11-03 09:13:32 +0100

    regression(fsm): file not committed

---
+++ src/api/fsm/EventType.ts
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Interacto.
+ * Interacto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Interacto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Interacto.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * The supported event types.
+ */
+export const eventTypes = [
+    "mousedown",
+    "mouseup",
+    "mousemove",
+    "keydown",
+    "keyup",
+    "click",
+    "auxclick",
+    "input",
+    "scroll",
+    "change",
+    "touchstart",
+    "touchend",
+    "touchmove"
+] as const;
+
+/**
+ * The supported event types as a type
+ */
+// eslint-disable-next-line @typescript-eslint/no-type-alias
+export type EventType = typeof eventTypes[number];
