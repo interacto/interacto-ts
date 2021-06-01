3b53a76522a21d2a5288b3339af66ce025ea5baa
Rémi Daniel <56882082+remi-daniel@users.noreply.github.com>
2021-06-01 17:24:34 +0200

    feat(command): Added new command TransferArrayItem (#14)
    Co-authored-by: Rémi Daniel <remi.daniel@insa-rennes.fr>

---
+++ src/impl/command/library/TransferArrayItem.ts
@@ -0,0 +1,124 @@
+/*
+ * This file is part of Interacto.
+ * Interacto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Interacto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Interacto.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {CommandBase} from "../CommandBase";
+import type {Undoable} from "../../../api/undo/Undoable";
+
+/**
+ * An undoable command that transfers an element from one array to another.
+ */
+export class TransferArrayItem<T> extends CommandBase implements Undoable {
+    /**
+     * The array to take the transferred element from.
+     */
+    private _srcArray: Array<T>;
+
+    /**
+     * The array to put the transferred element in.
+     */
+    private _tgtArray: Array<T>;
+
+    /**
+     * The index at which the element is located in the source array.
+     */
+    private _srcIndex: number;
+
+    /**
+     * The index at which the element must be put in the target array.
+     */
+    private _tgtIndex: number;
+
+    /**
+     * The name of the command.
+     */
+    private readonly cmdName: string;
+
+    /**
+     * Creates the command.
+     * @param srcArray - The array to take the transferred element from.
+     * @param tgtArray - The array to put the transferred element in.
+     * @param srcIndex - The index at which the element is located in the source array.
+     * @param tgtIndex - The index at which the element must be put in the destination array.
+     * @param cmdName - The name of the command.
+     */
+    public constructor(srcArray: Array<T>,
+                       tgtArray: Array<T>,
+                       srcIndex: number,
+                       tgtIndex: number,
+                       cmdName: string) {
+        super();
+        this._srcArray = srcArray;
+        this._tgtArray = tgtArray;
+        this._srcIndex = srcIndex;
+        this._tgtIndex = tgtIndex;
+        this.cmdName = cmdName;
+    }
+
+    protected execution(): void {
+        this.redo();
+    }
+
+    public canExecute(): boolean {
+        return (this._srcIndex >= 0 && this._srcIndex < this._srcArray.length) &&
+            (this._tgtIndex >= 0 && this._tgtIndex <= this._tgtArray.length);
+    }
+
+    public getUndoName(): string {
+        return this.cmdName;
+    }
+
+    public redo(): void {
+        const elt = this._srcArray[this._srcIndex];
+        this._srcArray.splice(this._srcIndex, 1);
+        this._tgtArray.splice(this._tgtIndex, 0, elt);
+    }
+
+    public undo(): void {
+        const elt = this._tgtArray[this._tgtIndex];
+        this._tgtArray.splice(this._tgtIndex, 1);
+        this._srcArray.splice(this._srcIndex, 0, elt);
+    }
+
+    public get srcArray(): Array<T> {
+        return this._srcArray;
+    }
+
+    public set srcArray(value: Array<T>) {
+        this._srcArray = value;
+    }
+
+    public get tgtArray(): Array<T> {
+        return this._tgtArray;
+    }
+
+    public set tgtArray(value: Array<T>) {
+        this._tgtArray = value;
+    }
+
+    public get srcIndex(): number {
+        return this._srcIndex;
+    }
+
+    public set srcIndex(value: number) {
+        this._srcIndex = value;
+    }
+
+    public get tgtIndex(): number {
+        return this._tgtIndex;
+    }
+
+    public set tgtIndex(value: number) {
+        this._tgtIndex = value;
+    }
+}
---
+++ test/command/library/TransferArrayItem.test.ts
@@ -0,0 +1,109 @@
+/*
+ * Interacto
+ * Copyright (C) 2019 Arnaud Blouin
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {TransferArrayItem} from "../../../src/impl/command/library/TransferArrayItem";
+
+let cmd: TransferArrayItem<number>;
+let array1: Array<number>;
+let array2: Array<number>;
+
+beforeEach(() => {
+    array1 = [0, 1, 2];
+    array2 = [3, 4];
+    cmd = new TransferArrayItem<number>(array1, array2, 0, 1, "Transfer array item");
+});
+
+test("testCanDo", () => {
+    expect(cmd.canExecute()).toBeTruthy();
+});
+
+test("testCannotDoSrcIndex1TooLow", () => {
+    cmd = new TransferArrayItem<number>(array1, array2, -1, 0, "Transfer array item");
+    expect(cmd.canExecute()).toBeFalsy();
+});
+
+test("testCannotDoSrcIndex1TooHigh", () => {
+    cmd = new TransferArrayItem<number>(array1, array2, 5, 0, "Transfer array item");
+    expect(cmd.canExecute()).toBeFalsy();
+});
+
+test("testCannotDoSrcIndex2TooLow", () => {
+    cmd = new TransferArrayItem<number>(array1, array2, 0, -1, "Transfer array item");
+    expect(cmd.canExecute()).toBeFalsy();
+});
+
+test("testCannotDoSrcIndex2TooHigh", () => {
+    cmd = new TransferArrayItem<number>(array1, array2, 0, 3, "Transfer array item");
+    expect(cmd.canExecute()).toBeFalsy();
+});
+
+test("testGetUndoName", () => {
+    expect(cmd.getUndoName()).toStrictEqual("Transfer array item");
+});
+
+test("testRedo", () => {
+    cmd.redo();
+    expect(array2[1]).toStrictEqual(0);
+    expect(array1[0]).toStrictEqual(1);
+});
+
+test("testExecution", async () => {
+    await cmd.execute();
+    expect(array2[1]).toStrictEqual(0);
+    expect(array1[0]).toStrictEqual(1);
+});
+
+test("testUndo", async () => {
+    await cmd.execute();
+    cmd.undo();
+    expect(array2[1]).toStrictEqual(4);
+    expect(array1[0]).toStrictEqual(0);
+});
+
+test("testGetSrcArray", () => {
+    expect(cmd.srcArray).toStrictEqual(array1);
+});
+
+test("testGetTgtArray", () => {
+    expect(cmd.tgtArray).toStrictEqual(array2);
+});
+
+test("testSetSrcArray", () => {
+    cmd.srcArray = array2;
+    expect(cmd.srcArray).toStrictEqual(array2);
+});
+
+test("testSetTgtArray", () => {
+    cmd.tgtArray = array1;
+    expect(cmd.tgtArray).toStrictEqual(array1);
+});
+
+test("testGetSrcIndex", () => {
+    expect(cmd.srcIndex).toStrictEqual(0);
+});
+
+test("testGetTgtIndex", () => {
+    expect(cmd.tgtIndex).toStrictEqual(1);
+});
+
+test("testSetSrcIndex", () => {
+    cmd.srcIndex = 2;
+    expect(cmd.srcIndex).toStrictEqual(2);
+});
+
+test("testSetTgtIndex", () => {
+    cmd.tgtIndex = 2;
+    expect(cmd.tgtIndex).toStrictEqual(2);
+});
