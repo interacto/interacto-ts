78ee33e723a17d2f4af82664f4b1a2865350106d
arnobl <1052622+arnobl@users.noreply.github.com>
2021-06-17 14:57:18 +0200

    feat(command): new command setProperty

---
+++ test/command/library/SetProperty.test.ts
@@ -0,0 +1,157 @@
+/*
+ * Interacto
+ * Copyright (C) 2019 Arnaud Blouin
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {SetProperty} from "../../../src/impl/command/library/SetProperty";
+
+class SecondStubSetProp {
+    public val: Array<number>;
+}
+
+class StubForSetProp {
+    public foo: number;
+
+    public bar: SecondStubSetProp;
+
+    private _foo2: string;
+
+    public get foo2(): string {
+        return this._foo2;
+    }
+
+    public set foo2(val: string) {
+        this._foo2 = val;
+    }
+}
+
+let obj: StubForSetProp;
+
+beforeEach(() => {
+    obj = new StubForSetProp();
+});
+
+describe("using a public number property", () => {
+    let cmd: SetProperty<StubForSetProp, "foo">;
+
+    beforeEach(() => {
+        cmd = new SetProperty(obj, "foo", 3);
+    });
+
+    test("execute works", async () => {
+        const res = await cmd.execute();
+        cmd.done();
+
+        expect(res).toBeTruthy();
+        expect(cmd.hadEffect()).toBeTruthy();
+        expect(obj.foo).toBe(3);
+    });
+
+    test("undo works", async () => {
+        obj.foo = 1;
+        await cmd.execute();
+        cmd.undo();
+        expect(obj.foo).toBe(1);
+    });
+
+    test("redo works", async () => {
+        obj.foo = 2;
+        await cmd.execute();
+        cmd.undo();
+        cmd.redo();
+        expect(obj.foo).toBe(3);
+    });
+
+    test("undo name", () => {
+        expect(cmd.getUndoName()).toBe("Set foo value");
+    });
+});
+
+describe("using a public string getter/setter", () => {
+    let cmd: SetProperty<StubForSetProp, "foo2">;
+
+    beforeEach(() => {
+        cmd = new SetProperty(obj, "foo2", "yolo");
+    });
+
+    test("execute works", async () => {
+        const res = await cmd.execute();
+        cmd.done();
+
+        expect(res).toBeTruthy();
+        expect(cmd.hadEffect()).toBeTruthy();
+        expect(obj.foo2).toBe("yolo");
+    });
+
+    test("undo works", async () => {
+        obj.foo2 = "fooo";
+        await cmd.execute();
+        cmd.undo();
+        expect(obj.foo2).toBe("fooo");
+    });
+
+    test("redo works", async () => {
+        obj.foo2 = "fooo22";
+        await cmd.execute();
+        cmd.undo();
+        cmd.redo();
+        expect(obj.foo2).toBe("yolo");
+    });
+
+    test("undo name", () => {
+        expect(cmd.getUndoName()).toBe("Set foo2 value");
+    });
+});
+
+describe("using a public object property", () => {
+    let cmd: SetProperty<StubForSetProp, "bar">;
+    let obj2: SecondStubSetProp;
+
+    beforeEach(() => {
+        obj2 = new SecondStubSetProp();
+        obj2.val = [1, 2, 3];
+        cmd = new SetProperty(obj, "bar", obj2);
+    });
+
+    test("execute works", async () => {
+        const res = await cmd.execute();
+        cmd.done();
+
+        expect(res).toBeTruthy();
+        expect(cmd.hadEffect()).toBeTruthy();
+        expect(obj.bar).toBe(obj2);
+    });
+
+    test("undo works", async () => {
+        const obj3 = new SecondStubSetProp();
+        obj3.val = [4, 5];
+        obj.bar = obj3;
+        await cmd.execute();
+        cmd.undo();
+        expect(obj.bar).toBe(obj3);
+    });
+
+    test("redo works", async () => {
+        const obj3 = new SecondStubSetProp();
+        obj3.val = [4, 5, 0];
+        obj.bar = obj3;
+        await cmd.execute();
+        cmd.undo();
+        cmd.redo();
+        expect(obj.bar).toBe(obj2);
+    });
+
+    test("undo name", () => {
+        expect(cmd.getUndoName()).toBe("Set bar value");
+    });
+});
