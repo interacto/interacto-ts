9150c8304bdcf49b67a3ff0878c9950351bac9e2
arnobl <1052622+arnobl@users.noreply.github.com>
2024-01-29 12:40:05 +0100

    feat(interaction): key interactions have a key parameter

--- test/interaction/library/KeyTyped.test.ts
+++ test/interaction/library/KeyTyped.test.ts
@@ -25,74 +25,115 @@ describe("using a key typed interaction", () => {
     let logger: Logger & MockProxy<Logger>;
 
     beforeEach(() => {
-        jest.useFakeTimers();
         handler = mock<FSMHandler>();
         logger = mock<Logger>();
-        interaction = new KeyTyped(logger);
-        interaction.fsm.addHandler(handler);
         text = document.createElement("textarea");
     });
 
-    test("type 'a' in the textarea starts and stops the interaction.", () => {
-        interaction.registerToNodes([text]);
-        robot(text)
-            .keepData()
-            .keydown({"code": "a"})
-            .keyup();
-        expect(handler.fsmStarts).toHaveBeenCalledTimes(1);
-        expect(handler.fsmStops).toHaveBeenCalledTimes(1);
+    afterEach(() => {
+        interaction.uninstall();
+        jest.clearAllMocks();
     });
 
-    test("log interaction is ok", () => {
-        interaction.log(true);
-        interaction.registerToNodes([text]);
-        robot(text)
-            .keepData()
-            .keydown({"code": "a"})
-            .keyup();
-
-        expect(logger.logInteractionMsg).toHaveBeenCalledTimes(5);
-    });
-
-    test("no log interaction is ok", () => {
-        interaction.registerToNodes([text]);
-        robot(text)
-            .keepData()
-            .keydown({"code": "a"})
-            .keyup();
-
-        expect(logger.logInteractionMsg).not.toHaveBeenCalled();
-    });
-
-    test("only press the key don't stop the interaction.", () => {
-        interaction.registerToNodes([text]);
-        robot(text)
-            .keydown({"code": "a"});
-        expect(handler.fsmStarts).not.toHaveBeenCalled();
-        expect(handler.fsmStops).not.toHaveBeenCalled();
-    });
-
-    test("if you release a key different that the one you press, the interaction don't stop", () => {
-        interaction.registerToNodes([text]);
-        robot(text)
-            .keydown({"code": "a"})
-            .keyup({"code": "b"});
-        expect(handler.fsmStarts).not.toHaveBeenCalled();
-        expect(handler.fsmStops).not.toHaveBeenCalled();
-    });
-
-    test("data is ok", () => {
-        const data = new KeyDataImpl();
-        interaction.registerToNodes([text]);
-        const newHandler = mock<FSMHandler>();
-        newHandler.fsmStops = jest.fn(() => {
-            data.copy(interaction.data);
+    describe("without the key", () => {
+        beforeEach(() => {
+            interaction = new KeyTyped(logger);
+            interaction.fsm.addHandler(handler);
+            interaction.registerToNodes([text]);
         });
-        interaction.fsm.addHandler(newHandler);
-        robot(text)
-            .keepData()
-            .keydown({"code": "z"})
-            .keyup();
-        expect(data.code).toBe("z");
+
+        test("type 'a' in the textarea starts and stops the interaction.", () => {
+            robot(text)
+                .keepData()
+                .keydown({"code": "a"})
+                .keyup();
+            expect(handler.fsmStarts).toHaveBeenCalledTimes(1);
+            expect(handler.fsmStops).toHaveBeenCalledTimes(1);
+        });
+
+        test("log interaction is ok", () => {
+            interaction.log(true);
+            robot(text)
+                .keepData()
+                .keydown({"code": "a"})
+                .keyup();
+
+            expect(logger.logInteractionMsg).toHaveBeenCalledTimes(5);
+        });
+
+        test("no log interaction is ok", () => {
+            robot(text)
+                .keepData()
+                .keydown({"code": "a"})
+                .keyup();
+
+            expect(logger.logInteractionMsg).not.toHaveBeenCalled();
+        });
+
+        test("only press the key don't stop the interaction.", () => {
+            robot(text)
+                .keydown({"code": "a"});
+            expect(handler.fsmStarts).not.toHaveBeenCalled();
+            expect(handler.fsmStops).not.toHaveBeenCalled();
+        });
+
+        test("if you release a key different that the one you press, the interaction don't stop", () => {
+            robot(text)
+                .keydown({"code": "a"})
+                .keyup({"code": "b"});
+            expect(handler.fsmStarts).not.toHaveBeenCalled();
+            expect(handler.fsmStops).not.toHaveBeenCalled();
+        });
+
+        test("data is ok", () => {
+            const data = new KeyDataImpl();
+            const newHandler = mock<FSMHandler>();
+            newHandler.fsmStops = jest.fn(() => {
+                data.copy(interaction.data);
+            });
+            interaction.fsm.addHandler(newHandler);
+            robot(text)
+                .keepData()
+                .keydown({"code": "z"})
+                .keyup();
+            expect(data.code).toBe("z");
+        });
+    });
+
+    describe("with the key", () => {
+        test("type the expected key works.", () => {
+            interaction = new KeyTyped(logger, "KeyQ");
+            interaction.fsm.addHandler(handler);
+            interaction.registerToNodes([text]);
+            robot(text)
+                .keepData()
+                .keydown({"code": "KeyQ"})
+                .keyup();
+            expect(handler.fsmStarts).toHaveBeenCalledTimes(1);
+            expect(handler.fsmStops).toHaveBeenCalledTimes(1);
+        });
+
+        test("type an unexpected key works.", () => {
+            interaction = new KeyTyped(logger, "KeyT");
+            interaction.fsm.addHandler(handler);
+            interaction.registerToNodes([text]);
+            robot(text)
+                .keepData()
+                .keydown({"code": "KeyI"})
+                .keyup();
+            expect(handler.fsmStarts).not.toHaveBeenCalled();
+        });
+
+        test("type expected Escape works.", () => {
+            interaction = new KeyTyped(logger, "Escape");
+            interaction.fsm.addHandler(handler);
+            interaction.registerToNodes([text]);
+            robot(text)
+                .keepData()
+                .keydown({"code": "Escape"})
+                .keyup();
+            expect(handler.fsmStarts).toHaveBeenCalledTimes(1);
+            expect(handler.fsmStops).toHaveBeenCalledTimes(1);
+        });
     });
 });
